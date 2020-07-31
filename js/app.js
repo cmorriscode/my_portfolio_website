998044aeb1766f9bd17c56ead2d28b7471886060
Charlie Morris <theboldedpixel@gmail.com>
2020-07-31 17:21:08 -0400

    Added functionality for hamburger popup

---
+++ js/app.js
@@ -0,0 +1,19 @@
+document
+  .querySelector(".nav-bar-open-slide-click")
+  .addEventListener("click", (e) => {
+    e.preventDefault;
+    const sideNav = document.querySelector(".side-nav");
+
+    document.querySelector(".btn-close").style.cssText = "display: block;";
+    sideNav.style.cssText = "width: 35rem;";
+  });
+
+document.querySelector(".btn-close").addEventListener("click", (e) => {
+  e.preventDefault;
+
+  const sideNav = document.querySelector(".side-nav");
+
+  sideNav.style.cssText = "width: 0";
+
+  e.target.style.display = "none";
+});
