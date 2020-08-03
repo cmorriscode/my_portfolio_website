3f7723cd1b93a44823a58e69380548cd28234bde
Charlie Morris <theboldedpixel@gmail.com>
2020-08-02 23:12:18 -0400

    Beginning to add projects page

--- js/app.js
+++ js/app.js
@@ -4,15 +4,16 @@ document
     e.preventDefault();
     const sideNav = document.querySelector(".side-nav");
 
-    document.querySelector(".btn-close").style.cssText = "display: block;";
+    document.querySelector(".btn-close").style.cssText = "display: block";
     sideNav.style.cssText = "width: 35rem;";
+    document.querySelector(".nav-bar-open-slide-click").style.display = "none";
   });
 
 document.querySelector(".btn-close").addEventListener("click", (e) => {
   e.preventDefault();
 
   const sideNav = document.querySelector(".side-nav");
-
+  document.querySelector(".nav-bar-open-slide-click").style.display = "block";
   sideNav.style.cssText = "width: 0";
 
   e.target.style.display = "none";
