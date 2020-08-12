20906b001f1b63649e1a4396fd6a60c54a8d18f8
Charles Morris <cmorriscode@gmail.com>
2020-08-11 21:34:40 -0400

    Made it so side nav bar closes whenever you click on any item for better UX. Changed pictures

--- js/app.js
+++ js/app.js
@@ -19,6 +19,14 @@ document.querySelector(".btn-close").addEventListener("click", (e) => {
   e.target.style.display = "none";
 });
 
+document.addEventListener("click", (e) => {
+  if (e.target.parentElement.classList.contains("side-nav")) {
+    document.querySelector(".side-nav").style.cssText = "width: 0";
+    document.querySelector(".btn-close").style.display = "none";
+    document.querySelector(".nav-bar-open-slide-click").style.display = "block";
+  }
+});
+
 AOS.init({
   offset: 125,
   duration: 1000,
